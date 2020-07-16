cccf72434490e8cfc67907b1d2a64cae5e083677
Fernando Santos Mazzoni <fernando.mazzoni@hotmail.com>
2020-07-15 21:56:32 -0300

    Desenvolvimento de aplicações com .Net
    Exercício 01 - Conheça o Entity framework  e crie aplicações ASP.NET

---
+++ Dev_App_ASP.NET/CursoMVC/CursoMVC/Models/Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace CursoMVC.Models
+{
+    public class Context : DbContext
+
+    {
+        public DbSet<Categoria> Categorias { get; set; }
+
+        public DbSet<Produto> Produtos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source = (localdb)\MSSQLLocalDB; Initial Catalog = CursoMVC; Integrated Security = True; Connect Timeout = 30; Encrypt = False; TrustServerCertificate = False; ApplicationIntent = ReadWrite; MultiSubnetFailover = False");
+        }
+    }
+}
