c0fa60feed627727f8e8ef8a47aa77561a3decb5
Fernando Santos Mazzoni <fernando.mazzoni@hotmail.com>
2020-07-19 12:56:20 -0300

    Curso desenvolvimento Asp.Net Core teste unitário.

--- Dev_App_ASP.NET/CursoMVC/CursoMVC/Models/Context.cs
+++ Dev_App_ASP.NET/CursoMVC/CursoMVC/Models/Context.cs
@@ -9,13 +9,18 @@ namespace CursoMVC.Models
     public class Context : DbContext
 
     {
-        public DbSet<Categoria> Categorias { get; set; }
+        public virtual DbSet<Categoria> Categorias { get; set; }
 
         public DbSet<Produto> Produtos { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer(@"Data Source = (localdb)\MSSQLLocalDB; Initial Catalog = CursoMVC; Integrated Security = True; Connect Timeout = 30; Encrypt = False; TrustServerCertificate = False; ApplicationIntent = ReadWrite; MultiSubnetFailover = False");
+        }
+
+        public virtual void SetModified(object entity)
+        {
+            Entry(entity).State = EntityState.Modified;
         }
     }
 }
--- Dev_App_ASP.NET/CursoMVC/CursoTest/CategoriasControllerTest.cs
+++ Dev_App_ASP.NET/CursoMVC/CursoTest/CategoriasControllerTest.cs
@@ -1,14 +1,13 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Moq;
+using System;
 using System.Collections.Generic;
 using System.Text;
-using Moq;
-using Microsoft.EntityFrameworkCore;
-using CursoMVC.Models;
-using DocumentFormat.OpenXml.Office2010.Excel;
-using Xunit;
 using CursoAPI.Controllers;
+using CursoMVC.Models;
+using Xunit;
 using System.Threading.Tasks;
-using DocumentFormat.OpenXml.Office2010.CustomUI;
+using System.Threading;
 
 namespace CursoTest
 {
@@ -18,28 +17,50 @@ namespace CursoTest
         private readonly Mock<Context> _mockContext;
         private readonly Categoria _categoria;
 
-
         public CategoriasControllerTest()
         {
             _mockSet = new Mock<DbSet<Categoria>>();
             _mockContext = new Mock<Context>();
             _categoria = new Categoria { Id = 1, Descricao = "Teste Categoria" };
 
-            _mockContext.Setup(m => m.Categorias).Returns(_mockSet.Object);
+            _mockContext.Setup(m => m.Categorias)
+                .Returns(_mockSet.Object);
 
             _mockContext.Setup(m => m.Categorias.FindAsync(1))
                 .ReturnsAsync(_categoria);
+
+            _mockContext.Setup(m => m.SetModified(_categoria));
+
+            _mockContext.Setup(m => m.SaveChangesAsync(It.IsAny<CancellationToken>()))
+                .ReturnsAsync(1);
         }
 
         [Fact]
         public async Task Get_Categoria()
         {
-           var service = new CategoriasController(_mockContext.Object);
+            var service = new CategoriasController(_mockContext.Object);
 
-           await service.GetCategoria(1);
+            await service.GetCategoria(1);
 
-            _mockSet.Verify(m => m.FindAsync(1),
-            Times.Once());
+            _mockSet.Verify(m => m.FindAsync(1), Times.Once());
+        }
+
+        [Fact]
+        public async Task Put_Categoria()
+        {
+            var service = new CategoriasController(_mockContext.Object);
+            await service.PutCategoria(1, _categoria);
+
+            _mockContext.Verify(m => m.SaveChangesAsync(It.IsAny<CancellationToken>()),
+                Times.Once());
+        }
+
+        [Fact]
+        public async Task Post_Categoria()
+        {
+            var service = new CategoriasController(_mockContext.Object);
+            await service.PostCategoria(_categoria);
         }
     }
 }
+  
